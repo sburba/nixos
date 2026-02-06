267613ea6f67ba649597acdbe634c547a76bcc94
Sam Burba <github@samburba.com>
2026-02-05 17:59:57 -0800

    Refactor flake to only specify common modules once

--- flake.nix
+++ flake.nix
@@ -4,61 +4,60 @@
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixos-25.11";
     nixpkgs-unstable.url = "github:nixos/nixpkgs/nixos-unstable";
+
     home-manager = {
       url = "github:nix-community/home-manager/release-25.11";
       inputs.nixpkgs.follows = "nixpkgs";
     };
+
     nixos-hardware.url = "github:NixOS/nixos-hardware/master";
   };
 
-  outputs = inputs@{ nixpkgs, nixpkgs-unstable, home-manager, nixos-hardware, ... }: {
+  outputs = inputs@{
+    nixpkgs,
+    nixpkgs-unstable,
+    home-manager,
+    nixos-hardware,
+    ...
+  }:
+  let
+    system = "x86_64-linux";
+
+    commonModules = [
+      ./configuration.nix
+      ./autoupdate.nix
+      ./1password.nix
+      ./steam.nix
+      ./jetbrains-toolbox.nix
+
+      home-manager.nixosModules.home-manager
+      {
+        home-manager.useGlobalPkgs = true;
+        home-manager.useUserPackages = true;
+        home-manager.users.sburba = ./home.nix;
+        home-manager.extraSpecialArgs = {
+          inherit nixpkgs-unstable;
+        };
+      }
+    ];
+
+    mkHost = extraModules:
+      nixpkgs.lib.nixosSystem {
+        inherit system;
+        modules = commonModules ++ extraModules;
+      };
+  in
+  {
     nixosConfigurations = {
-      sburba-laptop = nixpkgs.lib.nixosSystem {
-        system = "x86_64-linux";
-        modules = [
-          ./configuration.nix
-          ./hardware/laptop-hardware.nix
-          nixos-hardware.nixosModules.framework-amd-ai-300-series
-          ./laptop-wifi.nix
-          ./autoupdate.nix
-          ./1password.nix
-          ./steam.nix
-          home-manager.nixosModules.home-manager
-          {
-            home-manager.useGlobalPkgs = true;
-            home-manager.useUserPackages = true;
-            home-manager.users.sburba = ./home.nix;
+      sburba-laptop = mkHost [
+        ./hardware/laptop-hardware.nix
+        nixos-hardware.nixosModules.framework-amd-ai-300-series
+        ./laptop-wifi.nix
+      ];
 
-            # Optionally, use home-manager.extraSpecialArgs to pass
-            # arguments to home.nix
-            home-manager.extraSpecialArgs = {
-              inherit nixpkgs-unstable;
-            };
-          }
-        ];
-      };
-      sburba-desktop = nixpkgs.lib.nixosSystem {
-        system = "x86_64-linux";
-        modules = [
-          ./configuration.nix
-          ./hardware/desktop-hardware.nix
-          ./autoupdate.nix
-          ./1password.nix
-          ./steam.nix
-          home-manager.nixosModules.home-manager
-          {
-            home-manager.useGlobalPkgs = true;
-            home-manager.useUserPackages = true;
-            home-manager.users.sburba = ./home.nix;
-
-            # Optionally, use home-manager.extraSpecialArgs to pass
-            # arguments to home.nix
-            home-manager.extraSpecialArgs = {
-              inherit nixpkgs-unstable;
-            };
-          }
-        ];
-      };
+      sburba-desktop = mkHost [
+        ./hardware/desktop-hardware.nix
+      ];
     };
   };
 }

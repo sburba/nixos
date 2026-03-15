91e61c93fe6a42e135c1b62d4b718086b60a2c79
Sam Burba <github@samburba.com>
2026-03-15 12:45:03 -0700

    Reorder imports

--- flake.nix
+++ flake.nix
@@ -4,13 +4,12 @@
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixos-25.11";
     nixpkgs-unstable.url = "github:nixos/nixpkgs/nixos-unstable";
+    nixos-hardware.url = "github:NixOS/nixos-hardware/master";
 
     home-manager = {
       url = "github:nix-community/home-manager/release-25.11";
       inputs.nixpkgs.follows = "nixpkgs";
     };
-
-    nixos-hardware.url = "github:NixOS/nixos-hardware/master";
   };
 
   outputs = inputs@{
@@ -29,6 +28,7 @@
       ./1password.nix
       ./steam.nix
       ./jetbrains-toolbox.nix
+
 
       home-manager.nixosModules.home-manager
       {
